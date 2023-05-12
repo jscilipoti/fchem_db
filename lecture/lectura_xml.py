c91a244998efcd3a6245ec7498330b6da88db947
jscilipoti <jscilipoti@gmail.com>
2023-05-12 15:53:19 -0300

    viernes

--- lecture/lectura_xml.py
+++ lecture/lectura_xml.py
@@ -3,8 +3,18 @@ import xml.etree.ElementTree as ET
 root = ET.parse("lecture/ChemSep8.32.xml")
 root_node = root.getroot()
 
+#import ipdb
+#ipdb.set_trace()
+
 print (len(root_node))
 for child in root_node:
+    print (child.tag)
     for i in child:
-        #tag = i.tag
+        print(i.tag)
         print (i.attrib)
+        for j in i.attrib:
+            print (f"key: {j}")
+            print(f"valor: {i.attrib[j]}")
+        for 
+
+                
