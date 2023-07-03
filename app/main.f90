ca7b3b6b63a3c986908be86993111ee5d977bce6
jscilipoti <jscilipoti@gmail.com>
2023-07-03 11:40:26 -0300

    lectura_json

--- app/main.f90
+++ app/main.f90
@@ -1,6 +1,22 @@
 program main
-  use fchem_db, only: say_hello
-  implicit none
+  use json_module
 
-  call say_hello()
+  implicit none
+  type(json_file) :: json
+  logical :: found
+  real*8 :: i
+
+  ! initialize the class
+  call json%initialize()  
+  
+  ! read the file
+  call json%load(filename = 'lecture/db_json/Water.json')
+  
+  ! print the file to the console
+  call json%print()
+
+  call json%get('LibraryIndex.value', i, found)
+  if ( .not. found ) stop 1
+
+
 end program main
